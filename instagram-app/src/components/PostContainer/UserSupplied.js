0c4f097b091c2ca7abc13f21ca2b8a08060f4d6c
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-26 22:49:35 -0600

    Created a new file called UserSupplied.js
    This file will act as the user information (username and the image they used in the post)

---
+++ instagram-app/src/components/PostContainer/UserSupplied.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Posts.css';
+
+const PostHeader = props => {
+  return (
+    <div className="userSupplied">
+      <div className="USWrapper">
+        <img
+          alt="Image provided by user"
+          className="US-thumb"
+          src={thumbnailUrl}
+        />
+      </div>
+      <div>{username}</div>
+    </div>
+  );
+};
+
+export default PostHeader;
