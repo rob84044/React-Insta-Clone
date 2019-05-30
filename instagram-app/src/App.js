a4f350032358be870747a66abbff68fb68cecffb
Robert Pedersen <rob84044@users.noreply.github.com>
2019-05-29 22:28:20 -0600

    Created PostPage file, and migrated everything from app.js file to it
    I found that I had to restart the yarn server to get the renaming and moving of files to work. Will work on rest of MVP req\s tomrrow for Day 3 MVP

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,55 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import PostContainer from './components/PostContainer/PostContainer';
-import dummyData from './dummy-data';
+import PostPage from './components/PostContainer/PostPage';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      data: [],
-      search: '',
-      postsDuplicate: dummyData
-    };
-  }
-  componentDidMount() {
-    this.setState({ posts: dummyData });
-  }
-
-  searchHandler = e => {
-    this.setState({ search: e.target.value });
-  };
-
-  find = e => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    const found = this.state.posts.filter(post => {
-      if (
-        post.username.toLowerCase().includes(this.state.search.toLowerCase())
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    });
-    this.setState({ posts: found });
-    if (this.state.search == '') {
-      this.setState({ posts: this.state.postsDuplicate });
-    }
-  };
-
   render() {
     return (
-      <div className="App">
-        <SearchBar
-          search={this.state.search}
-          change={this.searchHandler}
-          find={this.find}
-        />
-        <PostContainer posts={this.state.posts} />
+      <div>
+        <PostPage />
       </div>
     );
   }
